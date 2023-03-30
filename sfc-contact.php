6abb7e6dd752015354bfbbccba62971e35e5366d
Mikael Fourré <mikael6287@live.fr>
2023-03-30 08:30:10 +0200

    Create the form view

--- sfc-contact.php
+++ sfc-contact.php
@@ -10,3 +10,11 @@ namespace Mf\SFC_Contact;
  */
 
 if ( ! function_exists( 'add_action' ) ) exit;
+
+define( 'SFC_CONTACT_PATH', plugin_dir_path( __FILE__ ) );
+define( 'SFC_CONTACT_URL', plugin_dir_url( __FILE__ ) );
+
+require_once SFC_CONTACT_PATH . 'classes/class-form.php';
+
+$form = new Form( SFC_CONTACT_PATH, SFC_CONTACT_URL );
+$form->init();
