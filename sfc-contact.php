31c525b66043694347f6e4cd439c93a888b9e318
Mikael Fourré <mikael6287@live.fr>
2023-03-30 07:59:39 +0200

    Setting up the plugin

---
+++ sfc-contact.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mf\SFC_Contact;
+
+/**
+ * Plugin Name: SFC Contact
+ * Description: This plugin offers the possibility to create a contact form on your website with a fully customizable code structure.
+ * Author: Mikael Fourré
+ * Version: 1.0.0
+ */
+
+if ( ! function_exists( 'add_action' ) ) exit;
